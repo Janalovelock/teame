62b50a1da0df28c7196fa3a9a2fddc39dda2af86
JordanPostak <jordan_postak@hotmail.com>
2023-09-26 11:06:13 -0600

    Completed Total in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,26 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // Check if there are items in the cart
+  if (cartItems && cartItems.length > 0) {
+    // Cart has items
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+    // Calculate total price
+    const total = cartItems.reduce((acc, item) => acc + item.FinalPrice, 0);
+
+    // Create HTML for total and insert it into the element
+    const totalHTML = `<p class="cart-total">Total: $${total.toFixed(2)}</p>`;
+    document.querySelector(".cart-footer").innerHTML = totalHTML;
+
+    document.querySelector(".cart-footer").classList.remove("hide");
+  } else {
+    // Cart is empty
+    document.querySelector(".product-list").innerHTML = "<p>Your cart is empty.</p>";
+    document.querySelector(".cart-footer").classList.add("hide");
+  }
 }
 
 function cartItemTemplate(item) {
@@ -19,7 +37,7 @@ function cartItemTemplate(item) {
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <p class="cart-card__price">$${item.FinalPrice.toFixed(2)}</p>
 </li>`;
 
   return newItem;
