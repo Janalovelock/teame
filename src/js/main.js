f78a4541c058e24e9e124069f32863a7c732056b
Nathaniel Dunham <dun19007@byui.edu>
2023-10-05 10:18:30 -0700

    proper formatting applied and image check.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import productList from "./productList.mjs";
 
-const container = document.querySelector(".product-list")
+const container = document.querySelector(".product-list");
 
 productList(container, "tents", 4);
