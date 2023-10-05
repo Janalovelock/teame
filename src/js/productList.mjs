3ea1d569182e00c6779451123196272ca115b1b6
Nathaniel Dunham <dun19007@byui.edu>
2023-10-05 10:18:01 -0700

    updated to check for valid image files.

---
+++ src/js/productList.mjs
@@ -0,0 +1,28 @@
+import { getData } from "./productData.mjs";
+import { renderList } from "./utils.mjs";
+
+export default async function productList(selector, category, limit){
+    const data = await getData(category)
+    let items = []
+    for(let i = 0; i < data.length; i++){
+        const imageBool = await fetch(data[i].Image).then((res) => (res.status))
+        if(imageBool == 200 && items.length < limit)
+            items.push(data[i])
+    }
+    renderList(productCardTemplate, selector, items)
+}
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+            <a href="product_pages/index.html?product=${product.Id}">
+            <img
+                src="${product.Image}"
+                alt="Image of ${product.Name}"
+            />
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.NameWithoutBrand}</h2>
+            <p class="product-card__price">$${product.ListPrice}</p></a>
+        </li>`
+}
+
+
