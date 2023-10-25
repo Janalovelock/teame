e61a91e27a89157ff84e260868132f43b98d593d
Janalovelock <janalovelock@gmail.com>
2023-10-25 11:49:43 -0500

    add welcome banner

---
+++ src/js/welcomeBanner.js
@@ -0,0 +1,19 @@
+// welcomeBanner.js
+// Get the modal
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+window.onload = function() {
+    modal.style.display = "block";
+}
+var registerButton = document.getElementById("registerButton");
+registerButton.onclick = function() {
+    //registration logic would go here
+}
