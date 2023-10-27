07ce0b37b13116c1a6d996fca302523f0e5a255e
JordanPostak <jordan_postak@hotmail.com>
2023-10-27 15:20:16 -0600

    partially finished until we get items to show

--- src/product-list/product-list.js
+++ src/product-list/product-list.js
@@ -1,3 +1,5 @@
+//This is responsible for handling the product list page. It imports functions and data from other files, loads a list of products, and updates the page based on user interactions.
+
 import productList from "../js/productList.mjs";
 import { loadHeaderFooter, getParam, renderList} from "../js/utils.mjs";
 import { loadAlerts } from "../js/alertModule.js";
@@ -6,9 +8,12 @@ import { productCardTemplate } from "../js/productList.mjs"; // Update the path 
 
 
 const container = document.querySelector(".product-list");
+const categoryBreadcrumb = document.getElementById("categoryBreadcrumb");
 
 let category = getParam("category");
 let prettyCategory = category[0].toUpperCase() + category.slice(1);
+
+document.getElementById('categoryBreadcrumb').textContent = prettyCategory;
 document.querySelector("title").textContent += prettyCategory;
 document.querySelector(".products h2").textContent += `: ${prettyCategory}`;
 
