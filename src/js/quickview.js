8206065966ed6c2c0b105d313827ccfd53fe9ebc
abdulmahl <mahalanguma87@gmail.com>
2023-11-15 10:49:19 +0200

    Added a comments section on the product detls page

--- src/js/quickview.js
+++ src/js/quickview.js
@@ -1,4 +1,7 @@
+import { setLocalStorage } from "./utils.mjs";
+
 const quickViewBtn = document.getElementById("quickViewBtn");
+const addCommentBtn = document.getElementById("addCommentBtn");
 const productModal = document.getElementById("product-modal");
 const closeMod = document.getElementsByClassName("close-modal")[0];
 const modalText = document.getElementById("modal-text");
@@ -10,3 +13,41 @@ quickViewBtn.onclick = function() {
 closeMod.onclick = function() {
     productModal.style.display = "none";
 }
+
+const comments = [{ 
+
+}];
+
+function displayComments() {
+    const commentsContainer = document.getElementById("comments-container");
+    
+    commentsContainer.innerText = '';
+
+    const productComments = comments.filter(comment => comment);
+    console.log(productComments);
+
+    productComments.forEach(comment => {
+        if(!comments) {
+            return;
+        }
+        const commentElement = document.createElement("p");
+        commentElement.textContent = comment.text;
+        commentsContainer.appendChild(commentElement);
+    });
+}
+
+const commentText = document.getElementById("comment-input");
+
+addCommentBtn.onclick = function() {
+    const newComment = {
+        text: commentText.value
+    };
+
+    comments.push(newComment);
+
+    displayComments();
+
+    commentText.value = "";
+};
+
+displayComments();
